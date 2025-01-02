85ab718f21519e2195128c6e3aa09193f6b9b9b9
Umutt <umutulay1907@gmail.com>
2025-01-01 19:45:40 -0600

    search with given topics

--- openreview.py
+++ openreview.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import sync_playwright
 
+user_input = input("Enter the topic you want to search for: ")
+
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)  # Set headless=False to see the browser window
     page = browser.new_page()
-    page.goto('http://playwright.dev')
-    # page.screenshot(path=f'example-{browser_type.name}.png')
+    page.goto(f'https://openreview.net/search?term={user_input}&group=all&content=all&source=all')
+    page.wait_for_timeout(15000)  # Time in milliseconds (15000 ms = 15 seconds)
     browser.close()
