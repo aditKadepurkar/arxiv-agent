c426e73b659901f8da8849cdcce493641baa908a
Umutt <umutulay1907@gmail.com>
2025-01-01 20:42:20 -0600

    async implemented

--- openreview.py
+++ openreview.py
@@ -1,10 +1,12 @@
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
 
 user_input = input("Enter the topic you want to search for: ")
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)  # Set headless=False to see the browser window
-    page = browser.new_page()
-    page.goto(f'https://openreview.net/search?term={user_input}&group=all&content=all&source=all')
-    page.wait_for_timeout(15000)  # Time in milliseconds (15000 ms = 15 seconds)
-    browser.close()
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)  # Set headless=False to see the browser window
+        page = await browser.new_page()
+        await page.goto(f'https://openreview.net/search?term={user_input}&group=all&content=all&source=all')
+        await page.wait_for_timeout(15000)  # Time in milliseconds (15000 ms = 15 seconds)
+        await browser.close()
+asyncio.run(main())
